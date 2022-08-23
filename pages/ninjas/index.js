136bffe47db194d86ba7155326ee61dc496e1763
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-08-23 07:32:00 -0700

    implementing end points

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,28 @@
+import Link from 'next/link';
+import styles from '../../styles/Ninjas.module.css'
+
+export const getStaticProps = async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  
+  return {
+    props: {ninjas: data}
+  }
+}
 
 
-const index = () => {
+const index = ({ninjas}) => {
+ 
   return (
     <div>
-      this are ninjas
+      <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </Link>
+      ))}
     </div>
   );
 }
