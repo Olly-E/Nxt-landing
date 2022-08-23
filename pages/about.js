7c862a7a9cb43051f4c83efd465ecb21406c47b2
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-08-23 01:37:30 -0700

    progress

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,20 @@
+import Head from "next/head";
+
+
 const about = () => {
   return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" cpntent="ninjas"/>
+      </Head>
+    
     <div>
         <h1>About</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi reprehenderit id accusamus pariatur dolor ullam hic, maiores explicabo harum quasi, vero cumque at dignissimos architecto quidem consectetur! Temporibus vitae aut quis ea autem iure reiciendis cupiditate modi distinctio quae deleniti repellendus officiis adipisci nam praesentium cumque quo similique nemo nobis, consequuntur, voluptatum doloremque eveniet eaque. Laudantium cumque corporis dicta ratione?</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam dignissimos, debitis molestiae inventore numquam ullam vitae earum nisi facere commodi fugit repudiandae esse provident architecto velit deserunt maxime corporis quo aperiam officiis temporibus necessitatibus. Saepe qui earum pariatur cupiditate quas! Rerum dolorem quisquam amet nemo quidem ullam expedita aperiam aspernatur.</p>
     </div>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1>HomePage</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sint eveniet provident id nisi architecto culpa officiis iusto ipsum optio amet maxime accusamus vero veritatis cupiditate non aliquam corrupti nihil saepe, illum odit. Sed, quam ad? Doloribus quod ullam consectetur, assumenda vero aliquid sapiente, earum ea, ex omnis quo asperiores quidem iure. Repudiandae fugiat temporibus dolorem reiciendis error maxime dolor!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quod nam quo est laboriosam ipsa, repellat quis distinctio alias dolore veniam nostrum ab quas illum culpa sit molestias maiores ipsam cumque? Nesciunt itaque deserunt earum id praesentium. Repellat maiores quas, porro delectus placeat laboriosam adipisci odit quos explicabo velit perspiciatis iste iusto hic nisi beatae excepturi asperiores nihil sint et!</p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name='keywords' content="ninjas"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>HomePage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sint eveniet provident id nisi architecto culpa officiis iusto ipsum optio amet maxime accusamus vero veritatis cupiditate non aliquam corrupti nihil saepe, illum odit. Sed, quam ad? Doloribus quod ullam consectetur, assumenda vero aliquid sapiente, earum ea, ex omnis quo asperiores quidem iure. Repudiandae fugiat temporibus dolorem reiciendis error maxime dolor!</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quod nam quo est laboriosam ipsa, repellat quis distinctio alias dolore veniam nostrum ab quas illum culpa sit molestias maiores ipsam cumque? Nesciunt itaque deserunt earum id praesentium. Repellat maiores quas, porro delectus placeat laboriosam adipisci odit quos explicabo velit perspiciatis iste iusto hic nisi beatae excepturi asperiores nihil sint et!</p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
+    
   )
 }
